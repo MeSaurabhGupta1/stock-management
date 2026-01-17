6c3ce1c64070c41c53cc40b243ba914ec22df050
saurabh-gupta26_infosys <saurabh.gupta26@infosys.com>
2026-01-17 12:31:50 +0530

    import fixes

--- src/components/filterControls/FilterControls.tsx
+++ src/components/filterControls/FilterControls.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { IoCashOutline, IoTrendingUp } from "react-icons/io5";
-import { MinCurrentPriceProps } from "./FilterControls.type";
+import {
+  MinCurrentPriceProps,
+  MinPercentageChangeProps,
+} from "./FilterControls.type";
 import styles from "../../App.module.css";
 import { MIN_NUMERIC_VALUE, NUMERIC_STEP } from "../../constants";
+import { parseFloatSafe } from "../../utils/numberUtils";
 
 export const MinCurrentPrice: React.FC<MinCurrentPriceProps> = React.memo(
   ({ value, onChange }) => {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      onChange(e.target.value === "" ? 0 : parseFloat(e.target.value));
+      onChange(e.target.value === "" ? 0 : parseFloatSafe(e.target.value));
     };
 
     return (
@@ -29,15 +33,10 @@ export const MinCurrentPrice: React.FC<MinCurrentPriceProps> = React.memo(
   }
 );
 
-export interface MinPercentageChangeProps {
-  value: number;
-  onChange: (value: number) => void;
-}
-
 export const MinPercentageChange: React.FC<MinPercentageChangeProps> =
   React.memo(({ value, onChange }) => {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      onChange(e.target.value === "" ? 0 : parseFloat(e.target.value));
+      onChange(e.target.value === "" ? 0 : parseFloatSafe(e.target.value));
     };
 
     return (
--- src/components/filterControls/FilterControls.type.ts
+++ src/components/filterControls/FilterControls.type.ts
@@ -2,3 +2,8 @@ export interface MinCurrentPriceProps {
   value: number;
   onChange: (value: number) => void;
 }
+
+export interface MinPercentageChangeProps {
+  value: number;
+  onChange: (value: number) => void;
+}
